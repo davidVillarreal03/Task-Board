2341b01b618de777bd7d11cb5381f5baeaced352
David Villarreal <djmarch92014@gmail.com>
2024-06-17 18:25:46 -0500

    I have finished the requirements for the challenge and have also added an image of my website

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # Task-Board
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+    My motivation was to be able to allow the user to plan out what they have to do using an online task board. 
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.") 
+    This project was built in the hopes of allowing a user to interact with a website that remember what they planned to due on a certain day and indicate what state that objective is in such as: late, about to be due and an indication of coming up in the future.
+- What problem does it solve?
+    This will help the user keep track of what they have planned for themselves without haveing to rely on paper or the own memory. If they are using their computer they can easily bookmark the website to reference it later when they decide.
+- What did you learn?
+   I was able to learn how to drag items and place them in different positions. I have also learned that day.js is a great source to reference the present day and make tasks revolve around what the current day is. This will be shown by what color the task card is.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+    [screenshot of my page](assets/images/screenshot.png)
+
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,161 @@
-// Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-const addTask = document.querySelector('#submit');
-let title = document.querySelector('#task-title');
-let dueDate = document.getElementById('due-date');
-let description = document.querySelector('#description');
+const title = $('#task-name-input');
+const dueDate = $('#taskDueDate');
+const description = $('#task-description-input');
+const submitForm = document.querySelector('#submit'); 
 
-$('#btn-save').click(function() {
-    $('#formModal').modal('hide');
- });
- $('#submit').click(function() {
-    $('#formModal').modal('hide');
- });  
+function readTasksFromStorage() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    
+    if (!tasks) {
+        tasks = [];
 
-addTask.addEventListener('click', function(){
-    title = title.value;
-    description = description.value; 
-    dueDate = dueDate.value;   
-});
+    }
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
-
+    return tasks;
 }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
+function saveTasksToStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
+function handleAddTask(event) {
+    event.preventDefault();
+
+    const taskName = title.val().trim();
+    const taskDescription = description.val().trim();
+    const taskDate = dueDate.val();
+  
+    const newTask = {
+      name: taskName,
+      description: taskDescription,
+      dueDate: taskDate,
+      status: 'to-do',
+    };
+
+    const tasks = readTasksFromStorage();
+    tasks.push(newTask);
+  
+    saveTasksToStorage(tasks);
+  
+    printTaskData();
+  
+   title.val('');
+   description.val('');
+   dueDate.val('');
     
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+function printTaskData() {
+    const tasks = readTasksFromStorage();
+  
+    const todoList = $('#todo-cards');
+    todoList.empty();
+  
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+  
+    const doneList = $('#done-cards');
+    doneList.empty();
+  
+    for (let task of tasks) {
+      if (task.status === 'to-do') {
+        todoList.append(createTaskCard(task));
+      } else if (task.status === 'in-progress') {
+        inProgressList.append(createTaskCard(task));
+      } else if (task.status === 'done') {
+        doneList.append(createTaskCard(task));
+      }
+    }
+  
+    $('.draggable').draggable({
+      opacity: 0.7,
+      zIndex: 100,
+      helper: function (e) {
+        const original = $(e.target).hasClass('ui-draggable')
+          ? $(e.target)
+          : $(e.target).closest('.ui-draggable');
+        return original.clone().css({
+          width: original.outerWidth(),
+        });
+      },
+    });
+  }
 
-}
+  function createTaskCard(task) {
+    const taskCard = $('<div>')
+      .addClass('card task-card draggable my-3')
+      .attr('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.description);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDeleteBtn = $('<button>')
+      .addClass('btn btn-danger delete')
+      .text('Delete')
+      .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
+  
+    if (task.dueDate && task.status !== 'done') {
+      const now = dayjs();
+      const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+  
+      if (now.isSame(taskDueDate, 'day')) {
+        taskCard.addClass('bg-warning text-white');
+      } else if (now.isAfter(taskDueDate)) {
+        taskCard.addClass('bg-danger text-white');
+        cardDeleteBtn.addClass('border-light');
+      }
+    }
+  
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+  
+    return taskCard;
+  }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event){
+  function handleDeleteTask() {
+    const taskId = $(this).attr('data-task-id');
+    const tasks = readTasksFromStorage();
+  
+    tasks.forEach((task) => {
+      if (task.id === taskId) {
+        tasks.splice(tasks.indexOf(task), 1);
+      }
+    });
+  
+    saveTasksToStorage(tasks);
+  
+    printTaskData();
+  }
 
-}
+  function handleDrop(event, ui) {
+    const tasks = readTasksFromStorage();
+  
+    const taskId = ui.draggable[0].dataset.taskId;
+  
+    const newStatus = event.target.id;
+  
+    for (let task of tasks) {
+      if (task.id === taskId) {
+        task.status = newStatus;
+      }
+    }
+    saveTasksToStorage(tasks);
+    printTaskData();
+  }
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+submitForm.addEventListener('click', handleAddTask)
 
-}
-
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
-
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
-});
+    printTaskData();
+  
+    $('#taskDueDate').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  
+    $('.lane').droppable({
+      accept: '.draggable',
+      drop: handleDrop,
+    });
+  });
