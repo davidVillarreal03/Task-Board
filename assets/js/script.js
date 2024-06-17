d805dfb6d78b15aa0c19b4162c09af9e4291668b
David Villarreal <djmarch92014@gmail.com>
2024-06-17 01:14:57 -0500

    Have made the modal and is responsive and stores information from the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const addTask = document.querySelector('#submit');
+let title = document.querySelector('#task-title');
+let dueDate = document.getElementById('due-date');
+let description = document.querySelector('#description');
+
+$('#btn-save').click(function() {
+    $('#formModal').modal('hide');
+ });
+ $('#submit').click(function() {
+    $('#formModal').modal('hide');
+ });  
+
+addTask.addEventListener('click', function(){
+    title = title.value;
+    description = description.value; 
+    dueDate = dueDate.value;   
+});
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -9,6 +26,7 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+
     
 }
 
